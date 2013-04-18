08d690e129ce77bc34675c2b26e1135bd2d0e66b
Giorgio Sintichakis <gsintichakis@gmail.com>
2013-04-18 17:21:00 -0300

    Create create.sh

---
+++ create.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+INSTANCE=$1
+PORT=$2
+
+if [ -z "$INSTANCE" -o -z "$PORT" ]; then
+  echo "Please provide unique instance name and port."
+	exit 1
+fi
+
+mkdir -p /var/redis/conf/instances
+cd /var/redis/conf/instances
+
+# Ensure the new instance name directory does not exist to avoid duplicates in the instances list
+if [ -d $INSTANCE ]; then
+	echo "Instance already exists. Please provide unique instance name and port."
+	exit 1
+fi
+
+echo -e "\nCreating new Redis instance for $INSTANCE:\n"
+
+echo -e "\tAdding instance name to instances list."
+
+echo $INSTANCE >> instances.txt
+
+# Create and enter new instance name directory
+mkdir -p $INSTANCE
+cd $INSTANCE
+
+echo -e "\tCreating config file for $INSTANCE instance with port $PORT."
+
+########## DO NOT TOUCH LINES BELOW ##########
+echo -e "### Include default Redis configuration.
+
+include /var/redis/conf/redis.conf
+
+### App-specific Redis settings.
+
+dir /var/redis/conf/instances/$INSTANCE/
+port $PORT" > conf
+########## DO NOT TOUCH LINES ABOVE ##########
+
+echo -e "\nDone creating new Redis instance."
+
+echo -e "\nPlease add new instance name and port to 'Redis Ports' spreadsheet in HitGrab's Google Drive (https://drive.google.com/a/hitgrab.com/#shared-with-me) to avoid conflicts.\n"
+
+exit 0
