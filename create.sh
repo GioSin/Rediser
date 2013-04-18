0ae1a4bee2ac62563bc6db12eadcb0e628f84a40
Giorgio Sintichakis <gsintichakis@gmail.com>
2013-04-18 19:35:42 -0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -8,7 +8,13 @@ if [ -z "$INSTANCE" -o -z "$PORT" ]; then
 	exit 1
 fi
 
+# Ensure instances directory exists
 mkdir -p /var/redis/conf/instances
+
+# Copy default Redis config into /var/redis/conf
+cp redis-default.conf /var/redis/conf/redis.conf
+
+# Move to instances directory
 cd /var/redis/conf/instances
 
 # Ensure the new instance name directory does not exist to avoid duplicates in the instances list
